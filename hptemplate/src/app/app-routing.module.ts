a983dd150597c7100dccf336ce6d1f48b791a201
t.ullrich <tobias.ullrich@beko-group.com>
2024-02-14 13:44:00 +0100

    first commit

---
+++ hptemplate/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+
+
+// import { Routes } from '@angular/router';
+// import { ImpressumComponent } from './impressum/impressum.component';
+// import { StartpageComponent } from './startpage/startpage.component';
+
+// export const routes: Routes = [
+//     { path: '', component: ImpressumComponent },
+//     { path: '', component: StartpageComponent },
+// ];
+
+
+//https://ullrich-tobias.de/
+//https://ullrich-tobias.de/UeberMich
+//https://ullrich-tobias.de/cranio-sacral/
+//https://ullrich-tobias.de/cranio-sacral/Diplomarbeit
+//https://ullrich-tobias.de/Astrologie/
+//https://ullrich-tobias.de/Astrologie/horoskope
+//https://ullrich-tobias.de/Kurse
+//https://ullrich-tobias.de/Kontakt
+//https://ullrich-tobias.de/impressum
