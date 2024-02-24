c12329bd59c2f9c9a480786d6bf816c2bc849d9f
Tobias Ullrich <ullrich.tobias@gmx.de>
2024-02-24 03:56:11 +0100

    menuscheisse

--- hptemplate/src/app/navmenu/navmenu.component.ts
+++ hptemplate/src/app/navmenu/navmenu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navmenu',
@@ -29,28 +30,97 @@ export class NavmenuComponent {
 
   indexOfPositionOfLogo: number = 3; //Ändern wenn das Logo anders positioniert werden soll  
 
-  subMenuRoutes: string[] =[
-    '/Kurse/Kursinhalte',
+  subMenuRoutes1: string[] =[
     '/Astrologie/Horoskope',
     '/Cranio-Sacral/Diplomarbeit',
+    '/Kurse/Kursinhalte',
+    '',
+    '',
+    '/Astrologie/Horoskope',
+    '',
    ];
 
-  subMenuDisplay: string[] =[
-    'Kursinhalte',
+  subMenuDisplay1: string[] =[
     'Horoskope',
     'Diplomarbeit',
+    'Kursinhalte',
+    '',
+    '',
+    'Horoskope',
+    '',
    ];
 
-   correspondingMainRoute: string[] =[
-    '/Astrologie',
-    '/Cranio-Sacral',
-    '/Kurse',
+   subMenuRoutes2: string[] =[
+    '',
+    '',
+    '',
+    '',
+    '',
+    '/Cranio-Sacral/Diplomarbeit',
+    '',
    ];
 
+  subMenuDisplay2: string[] =[
+    '',
+    '',
+    '',
+    '',
+    '',
+    'Diplomarbeit',
+    '',
+   ];
 
-   showSubMenu(id: number){
-    console.log('gehovert ' + id);
+   subMenuRoutes3: string[] =[
+    '',
+    '',
+    '',
+    '',
+    '',
+    '/Kurse/Kursinhalte',
+    '',
+   ];
+
+  subMenuDisplay3: string[] =[
+    '',
+    '',
+    '',
+    '',
+    '',
+    'Kursinhalte',
+    '',
+   ];
+
+   ausklappen(id: string){
+    console.log('ausklappen ' + id);
+    // (<HTMLInputElement>document.getElementById(id.toString())).classList.add('klappaus');
     (<HTMLInputElement>document.getElementById(id.toString())).classList.remove('d-none');
    }
+
+    einklappen(id: string){
+      console.log('einklappen ' + id);
+    // (<HTMLInputElement>document.getElementById(id.toString())).classList.remove('klappaus');
+    (<HTMLInputElement>document.getElementById(id.toString())).classList.add('d-none');
+   }
+
+
+   @HostListener('document:mouseenter', ['$event'])
+    onDocumentClick(event: MouseEvent) {
+    let elementId: string = (event.target as Element).id;
+    console.log('id: ',elementId);
+    console.log('id erste 4:', elementId.substring(1,4));
+    console.log('id nach 5:', elementId.substring(5,elementId.length));
+
+    if(elementId.substring(1,4) == 'main'){
+      console.log(event.target);
+      (<HTMLInputElement>document.getElementById('sub'+elementId.substring(5,elementId.length))).classList.remove('d-none');
+
+      //this.closeVideo();
+      // (<HTMLInputElement>document.getElementById('s1')).classList.remove('d-none');
+      
+      }
+   }
+   
+
+
 
 }
