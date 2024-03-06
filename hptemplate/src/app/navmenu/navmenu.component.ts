6e1f9b3e993259ab0c3ee0e8cd4476421ffa5c53
t.ullrich <tobias.ullrich@beko-group.com>
2024-03-06 16:51:43 +0100

    nav-menue finally solved

--- hptemplate/src/app/navmenu/navmenu.component.ts
+++ hptemplate/src/app/navmenu/navmenu.component.ts
@@ -100,34 +100,26 @@ constructor(private eleRef: ElementRef){}
     //console.log('id erste 4 Stellen:', elementId.substring(0,4)); //Befindet sich der Mauszeiger über einem main-menü-punkt?
     //console.log('id nach ab 5ter Stelle:', elementId.substring(4,elementId.length)); //Id die nach 'main' kommt, bei 'main1' ist es '1'
 
-    //Wenn ein Main-HTML-Element gehovert wird, dann mache das Untermenü (hat die id=4, wenn main4 gehovert wird) sichtbar
+    //[1.] Wenn ein Main-HTML-Element gehovert wird, dann mache das Untermenü (hat die id=4, wenn main4 gehovert wird) sichtbar
     if(elementId.substring(0,4) == 'main'){
      
-      //Prüfe das Array mit den Menü-Punkten
+      //Gehe durch das Array mit den Menü-Punkten
       for(let i=0;i<this.routes.length;i++){
-        //Entferne 'd-none'-Klasse beim zugehörigen Untermenü 
+        //[1.1] Entferne 'd-none'-Klasse beim zugehörigen Untermenü, damit es sichtbar wird 
         if(elementId.substring(4,elementId.length)==i.toString()){
           (<HTMLInputElement>document.getElementById(elementId.substring(4,elementId.length))).classList.remove('d-none');
         }
         else{
-          //Füge bei allen anderen Untermenüs die 'd-none'-Klasse hinzu, AUßER beim Menüpunkt mit dem Logo ('indexOfPositionOfLogo')
+        //[1.2] Füge bei allen anderen Untermenüs die 'd-none'-Klasse hinzu, damit sie unsichtbar bleiben, AUßER beim Menüpunkt mit dem Logo ('indexOfPositionOfLogo')
           if (i != this.indexOfPositionOfLogo){(<HTMLInputElement>document.getElementById(i.toString())).classList.add('d-none')};
         }
       };     
     };
+    //[2.] Wenn ein transparentes Element 'invXX' oder 'topX' gehovert wird, dann füge bei allen Untermenüs die 'd-none'-Klasse hinzu, damit sie unsichtbar werden, AUßER beim Menüpunkt mit dem Logo ('indexOfPositionOfLogo')
+    //'invXX' hängt immer als letzter Menüpunkt in allen Untermenüs unten dran; 'topX' steht für die Box in der sich ein Hauptmenüpunkt befindet
+    if(elementId.substring(0,3) == 'inv' || elementId.substring(0,3) == 'top'){
+      for(let i=0;i<this.routes.length;i++){if(i != this.indexOfPositionOfLogo){(<HTMLInputElement>document.getElementById(i.toString())).classList.add('d-none')}};
+    }
    }
-
-
-   //In Angular-Komponenten wird this.eleRef normalerweise durch die ElementRef-Klasse bereitgestellt, die eine Referenz auf das Host-Element der Komponente enthält. 
-   //Das nativeElement-Attribut wird dann verwendet, um auf das tatsächliche DOM-Element zuzugreifen.
-   MouseLeavingTheGlobalContainer(event: MouseEvent): boolean{
-    let element: HTMLElement = event.target as HTMLElement;
-    console.log('ID des verlassenen Elements ',element.id);
-    console.log('HTML-Element dieser Komponente ',(this.eleRef.nativeElement as HTMLElement));
-    console.log('Ist das verlassene Element Bestandteil dieser Komponente? ',(this.eleRef.nativeElement as HTMLElement).contains(element));
-    if(element.id=='xxxx'){for(let i=0;i<this.routes.length;i++){if(i != this.indexOfPositionOfLogo){(<HTMLInputElement>document.getElementById(i.toString())).classList.add('d-none')}}};
-   return (this.eleRef.nativeElement as HTMLElement).contains(element);
-  }
-
 
 }
