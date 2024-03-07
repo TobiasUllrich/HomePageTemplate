c33020004df85a3183f011ae83429dbb1b5b702b
t.ullrich <tobias.ullrich@beko-group.com>
2024-03-07 16:52:21 +0100

    scrolling issues

--- hptemplate/src/app/startpage/startpage.component.ts
+++ hptemplate/src/app/startpage/startpage.component.ts
@@ -31,25 +31,64 @@ export class StartpageComponent {
 
    @HostListener("window:scroll", ["$event"])
    onWindowScroll() {
-   //In chrome and some browser scroll is given to body tag
-   let pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
-   let max = document.documentElement.scrollHeight;
+   //[document] Dies ist das globale Objekt in JavaScript, das das gesamte *HTML-Dokument* repräsentiert.
+   //[documentElement] Dies ist eine Eigenschaft des document-Objekts, die das <html>-Element im *HTML-Dokument* repräsentiert.
+   //[body] Statt documentElement, weil manche Browser nur so funktionieren
 
 
-   //An element's scrollTop value is a measurement of the distance from the element's top to its topmost visible content. 
-   //When an element's content does not generate a vertical scrollbar, then its scrollTop value is 0.
+   //Diese Zeile verwendet document.documentElement.scrollTop, fällt aber automatisch auf document.body.scrollTop zurück, wenn document.documentElement.scrollTop nicht definiert ist (z. B. in älteren Versionen von Internet Explorer). 
+   //Auf diese Weise erhältst du den vertikalen Scroll-Offset unabhängig vom verwendeten Browser.
+   let distanceToUpperBorder = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+   let heightOfWholeDocument = document.documentElement.scrollHeight;
+   let visibleHeightOfBrowserWindow = window.innerHeight; 
+
+   let imageRect = (<HTMLElement>document.getElementById('myimage1')).getBoundingClientRect(); //#myImage
+   console.log(imageRect);
+   //https://stackoverflow.com/questions/11805955/how-to-get-the-distance-from-the-top-for-an-element
+
+
+   //[document.documentElement.scrollTop]
+   //Der scrollTop Value eines Elementes misst den Abstand in Pixeln zwischen dem obersten Rand des Dokuments und dem obersten Rand des sichtbaren Bereichs 
+   //Wenn ein Element keine vertikale Scrollbar generiert, dann ist sein scrollTop Value 0
    //console.log(document.documentElement.scrollTop);
 
-   //Höhe eines Elements inklusive vertikales Padding & Borders
+   //[offsetHeight] 
+   //Höhe eines Elements inklusive vertikales Padding & Borders -> muss draufgerechnet werden um den richtigen Abstand zu messen
    //console.log(document.documentElement.offsetHeight);
 
-   //The Element.scrollHeight read-only property is a measurement of the height of an element's content, including content not visible on the screen due to overflow.
+   //[scrollHeight]
+   //Dies ist eine Eigenschaft des documentElement, die die gesamte Höhe des gesamten HTML-Dokuments in Pixeln zurückgibt, einschließlich des sichtbaren und nicht sichtbaren Inhalts
    //console.log(document.documentElement.scrollHeight);
 
-   // pos/max will give you the distance between scroll bottom and and bottom of screen in percentage.
-   if(pos == max )   {
+   //[window.innerHeight]
+   //Höhe des sichtbaren Bereichs des Browserfensters 
+
+   //Position des unteren sichtbaren Rands im Browserfenster relativ zum oberen Rand des Dokuments.
+   //console.log(((document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight) + window.innerHeight);
+
+
+   // (distanceToUpperBorder/heightOfWholeDocument) will give you the distance between scroll bottom and and bottom of screen in percentage.
+   if(distanceToUpperBorder == heightOfWholeDocument )   {
     //Do your action here
+    //console.log(distanceToUpperBorder/heightOfWholeDocument);
     }
    }
 
 }
+
+
+
+//ELEREF FÜR DOKU
+
+// constructor(private eleRef: ElementRef){}
+
+// <div id="x" (mouseout)="MouseLeavingTheGlobalContainer($event)">
+
+//    //In Angular-Komponenten wird this.eleRef normalerweise durch die ElementRef-Klasse bereitgestellt, die eine Referenz auf das Host-Element der Komponente enthält. 
+//    //Das nativeElement-Attribut wird dann verwendet, um auf das tatsächliche DOM-Element zuzugreifen.
+//    MouseLeavingTheGlobalContainer(event: MouseEvent): boolean{
+//     let element: HTMLElement = event.target as HTMLElement;
+//     console.log('ID des verlassenen Elements ',element.id);
+//     console.log('HTML-Element dieser Komponente ',(this.eleRef.nativeElement as HTMLElement));
+//     console.log('Ist das verlassene Element Bestandteil dieser Komponente? ',(this.eleRef.nativeElement as HTMLElement).contains(element));
+//   }
