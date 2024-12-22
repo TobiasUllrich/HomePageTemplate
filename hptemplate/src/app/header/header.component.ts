a35726472551b15468c237c868f9c2099d9e1ae6
Tobias Ullrich <ullrich.tobias@gmx.de>
2024-12-22 22:12:10 +0100

    rdy for hosting

--- hptemplate/src/app/header/header.component.ts
+++ hptemplate/src/app/header/header.component.ts
@@ -5,28 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
+
 export class HeaderComponent {
 
-
-
   getNotificationClosed(evt: any) {
-      // Do something with the notification (evt) sent by the child!
-      console.log('CLOSSSSSSSEEEEEDDDDDDDDD');
+      console.log('Mobil-Menü wurde geschlossen');
 
       //Sorgt dafür, dass das Menü nicht schon beim Einklappen seine ganze Höhe verliert
       setTimeout(function(){
         (<HTMLInputElement>document.getElementById('hdr')).classList.remove('hdr100vh');
        }, 500);
 
-
       (<HTMLInputElement>document.getElementById('hdr')).classList.remove('posfxd');
   }
 
   getNotificationOpened(evt: any) {
-    // Do something with the notification (evt) sent by the child!
-    console.log('OPPPPEEEEENNNNNNEEEEEDDDDD');
+    console.log('Mobil-Menü wurde geöffnet');
     (<HTMLInputElement>document.getElementById('hdr')).classList.add('hdr100vh');
     (<HTMLInputElement>document.getElementById('hdr')).classList.add('posfxd');
-}
+  }
 
 }
--- hptemplate/src/app/navmenu/navmenu.component.ts
+++ hptemplate/src/app/navmenu/navmenu.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, HostListener, Output } from '@angular/core';
-import { Observable, auditTime, debounceTime, from, interval, map, of, timeout } from 'rxjs';
 
 @Component({
   selector: 'app-navmenu',
@@ -93,12 +92,13 @@ export class NavmenuComponent {
    
 constructor(){}
 
-
+//Info an app-header senden, dass das Menü geschlossen wird
 @Output() notifyParentClosed: EventEmitter<any> = new EventEmitter();
 sendMenuClosedNotification() {
     this.notifyParentClosed.emit('Some value to send to the parent');
 }
 
+//Info an app-header senden, dass das Menü geöffnet wird
 @Output() notifyParentOpened: EventEmitter<any> = new EventEmitter();
 sendMenuOpenedNotification() {
     this.notifyParentOpened.emit('Some value to send to the parent');
@@ -107,7 +107,7 @@ sendMenuOpenedNotification() {
    @HostListener('document:mouseover', ['$event'])
     onDocumentHover(event: MouseEvent) {
     
-      let elementId: string = (event.target as Element).id;
+    let elementId: string = (event.target as Element).id;
     //console.log('id: ',elementId); //Id des Elements, z.B. 'main1'
     //console.log('id erste 4 Stellen:', elementId.substring(0,4)); //Befindet sich der Mauszeiger über einem main-menü-punkt?
     //console.log('id nach ab 5ter Stelle:', elementId.substring(4,elementId.length)); //Id die nach 'main' kommt, bei 'main1' ist es '1'
@@ -137,83 +137,29 @@ sendMenuOpenedNotification() {
 
 
 
-  /**
-   * Opens the Mobile Menu from the right and plays the Cross-Symbol-Animation
-   */
+/**
+  * Opens the Mobile Menu from the right
+  */
   openMobileMenu(){
-    console.log('OPEN');
-    // (<HTMLInputElement>document.getElementById('mobile-menu')).classList.remove('height10vh');
+    //console.log('OPEN MOBILE-MENU');
     (<HTMLInputElement>document.getElementById('mobile-menu')).classList.add('openMobileMenu');
     (<HTMLInputElement>document.getElementById('mobile-menu')).classList.remove('closeMobileMenu');
-
-    // console.log('hallo klappmenü');
-    // console.log(window.innerWidth);
-    //   //Gehe durch das Array mit den Menü-Punkten
-    //   for(let i=0;i<3;i++){
-    //     console.log(`${i} ${(<HTMLInputElement>document.getElementById(`${i}`))}`);
-    //     //[1.1] Entferne 'd-none'-Klasse beim zugehörigen Untermenü, damit es sichtbar wird 
-    //       (<HTMLInputElement>document.getElementById(`${i}`)).classList.remove('d-none');
-    //       (<HTMLInputElement>document.getElementById(`${i}`)).classList.add('d-flex');
-    //     }
-
-    // (<HTMLInputElement>document.getElementById('mobile-menu')).classList.add('header-links-main-nav-mobile');
-    // (<HTMLInputElement>document.getElementById('mobile-menu')).classList.remove('header-links-main-nav');
-
-    // (<HTMLInputElement>document.getElementById('mobile-menu')).classList.add('header-links-mobile');
-    // (<HTMLInputElement>document.getElementById('mobile-menu')).classList.remove('header-links');
-    // (<HTMLInputElement>document.getElementById('upperLine')).classList.remove('openMobileMenuDisolveCrossUpperLine');
-    // (<HTMLInputElement>document.getElementById('lowerLine')).classList.remove('openMobileMenuDisolveCrossLowerLine');
-    // (<HTMLInputElement>document.getElementById('upperLine')).classList.add('openMobileMenuBuildCrossUpperLine');
-    // (<HTMLInputElement>document.getElementById('lowerLine')).classList.add('openMobileMenuBuildCrossLowerLine');
   }
 
 /**
- * Closes the Mobile Menu from the right and plays the Cross-Symbol-Animation backwards
+ * Closes the Mobile Menu to the left
  */
   closeMobileMenu(){
-    console.log('CLOSE');
+    //console.log('CLOSE MOBILE-MENU');
     (<HTMLInputElement>document.getElementById('mobile-menu')).classList.add('closeMobileMenu');
     (<HTMLInputElement>document.getElementById('mobile-menu')).classList.remove('openMobileMenu');
-    // (<HTMLInputElement>document.getElementById('mobile-menu')).classList.add('height10vh');
 
+    //Scrollt immer nach oben, falls man bei geöffnetem Klappmenü nach unten gescrollt hat
     window.scroll({
       top: 0, 
       left: 0, 
       behavior: 'smooth' 
     });
-    
-    // (<HTMLInputElement>document.getElementById('mobile-menu')).classList.add('header-links-main-nav');
-    // (<HTMLInputElement>document.getElementById('mobile-menu')).classList.remove('header-links-main-nav-mobile');
-    
-    // (<HTMLInputElement>document.getElementById('mobile-menu')).classList.add('header-links');
-    // (<HTMLInputElement>document.getElementById('mobile-menu')).classList.remove('header-links-mobile');
-
-    // (<HTMLInputElement>document.getElementById('upperLine')).classList.remove('openMobileMenuBuildCrossUpperLine');
-    // (<HTMLInputElement>document.getElementById('lowerLine')).classList.remove('openMobileMenuBuildCrossLowerLine');
-    // (<HTMLInputElement>document.getElementById('upperLine')).classList.add('openMobileMenuDisolveCrossUpperLine');
-    // (<HTMLInputElement>document.getElementById('lowerLine')).classList.add('openMobileMenuDisolveCrossLowerLine');
   }
-
-
-
-
-  // test?: Observable<Number>;
-
-  // ngOnInit(){
-  //   this.probieren();
-  // }
-
-  // probieren(){
-  //   let test = of(1,2,3,4).subscribe((r) => console.log('Of-Observable: ' + r));
-  //   let test2 = interval(1).pipe(auditTime(3000)).subscribe((r) => console.log('Interval-Observable: ' + r));
-
-  //   test3: Observable;
-  //   const test3 = interval(1000).pipe(map(x => x*10));
-
-  //   //test.unsubscribe();
-  //   //test2.unsubscribe();
-  // }
-
-
 
 }
